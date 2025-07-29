9ab3828f49fcec925b7e4100792feda0bb590256
Tiago <tiagoeweber@gmail.com>
2025-07-29 18:04:46 -0300

    Implementada camada de Infraestrutura em Arquivos

---
+++ ControleDeBar.Infraestrutura.Arquivos/Compartilhado/ContextoDados.cs
@@ -0,0 +1,68 @@
+﻿using ControleDeBar.Dominio.ModuloConta;
+using ControleDeBar.Dominio.ModuloGarcom;
+using ControleDeBar.Dominio.ModuloMesa;
+using ControleDeBar.Dominio.ModuloProduto;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ControleDeBar.Infraestrutura.Arquivos.Compartilhado;
+
+public class ContextoDados
+{
+    public List<Mesa> Mesas { get; set; } = new List<Mesa>();
+    public List<Garcom> Garcons { get; set; } = new List<Garcom>();
+    public List<Produto> Produtos { get; set; } = new List<Produto>();
+    public List<Conta> Contas { get; set; } = new List<Conta>();
+
+    private string pastaArmazenamento = "C:\\temp";
+    private string arquivoArmazenamento = "dados-controle-bar.json";
+
+    public ContextoDados() { }
+
+    public ContextoDados(bool carregarDados)
+    {
+        if (carregarDados)
+            Carregar();
+    }
+
+    public void Salvar()
+    {
+        // C:\temp\dados-controle-bar.json
+        string caminhoCompleto = Path.Combine(pastaArmazenamento, arquivoArmazenamento);
+
+        JsonSerializerOptions jsonOptions = new JsonSerializerOptions();
+        jsonOptions.WriteIndented = true;
+        jsonOptions.ReferenceHandler = ReferenceHandler.Preserve;
+
+        var jsonString = JsonSerializer.Serialize(this);
+
+        if (!Directory.Exists(pastaArmazenamento))
+            Directory.CreateDirectory(pastaArmazenamento);
+
+        File.WriteAllText(caminhoCompleto, jsonString);
+    }
+
+    public void Carregar()
+    {
+        string caminhoCompleto = Path.Combine(pastaArmazenamento, arquivoArmazenamento);
+
+        if (!File.Exists(caminhoCompleto)) return;
+
+        string jsonString = File.ReadAllText(caminhoCompleto);
+
+        if (string.IsNullOrWhiteSpace(jsonString)) return;
+
+        JsonSerializerOptions jsonOptions = new JsonSerializerOptions();
+        jsonOptions.ReferenceHandler = ReferenceHandler.Preserve;
+
+        ContextoDados? contextoArmazenado = JsonSerializer.Deserialize<ContextoDados>(jsonString);
+
+        if (contextoArmazenado == null) return;
+
+        Mesas = contextoArmazenado.Mesas;
+        Garcons = contextoArmazenado.Garcons;
+        Produtos = contextoArmazenado.Produtos;
+        Contas = contextoArmazenado.Contas;
+    }
+}
---
+++ ControleDeBar.Infraestrutura.Arquivos/Compartilhado/RepositorioBaseEmArquivo.cs
@@ -0,0 +1,80 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.Arquivos.Compartilhado;
+
+public abstract class RepositorioBaseEmArquivo<Tipo> where Tipo : EntidadeBase<Tipo>
+{
+    protected ContextoDados contextoDados;
+    protected List<Tipo> registros = new List<Tipo>();
+    protected int contadorRegistros = 0;
+    protected int contadorIds = 0;
+
+    protected RepositorioBaseEmArquivo(ContextoDados contextoDados)
+    {
+        this.contextoDados = contextoDados;
+    }
+
+    public void CadastrarRegistro(Tipo novoRegistro)
+    {
+        novoRegistro.Id = ++contadorIds;
+
+        registros.Add(novoRegistro);
+
+        contextoDados.Salvar();
+    }
+
+    public bool EditarRegistro(int idSelecionado, Tipo registroAtualizado)
+    {
+        Tipo registroSelecionado = SelecionarRegistroPorId(idSelecionado);
+
+        if (registroSelecionado == null)
+            return false;
+
+        registroSelecionado.AtualizarRegistro(registroAtualizado);
+
+        contextoDados.Salvar();
+
+        return true;
+    }
+
+    public bool ExcluirRegistro(int idSelecionado)
+    {
+        for (int i = 0; i < registros.Count; i++)
+        {
+            if (registros[i] == null)
+                continue;
+
+            else if (registros[i].Id == idSelecionado)
+            {
+                registros[i] = null;
+
+                contextoDados.Salvar();
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public List<Tipo> SelecionarRegistros()
+    {
+        return registros;
+    }
+
+    public Tipo SelecionarRegistroPorId(int idSelecionado)
+    {
+        for (int i = 0; i < registros.Count; i++)
+        {
+            Tipo registro = registros[i];
+
+            if (registro == null)
+                continue;
+
+            if (registro.Id == idSelecionado)
+                return registro;
+        }
+
+        return null;
+    }
+}
