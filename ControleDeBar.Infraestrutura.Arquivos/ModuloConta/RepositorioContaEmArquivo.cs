c758d3f0d340422a8f16db88ddbb965c643c7cad
Tiago <tiagoeweber@gmail.com>
2025-08-05 21:02:51 -0300

    Implementado repositório conta em arquivo

---
+++ ControleDeBar.Infraestrutura.Arquivos/ModuloConta/RepositorioContaEmArquivo.cs
@@ -0,0 +1,55 @@
+﻿using ControleDeBar.Dominio.ModuloConta;
+using ControleDeBar.Infraestrutura.Arquivos.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.Arquivos.ModuloConta;
+
+public class RepositorioContaEmArquivo : RepositorioBaseEmArquivo<Conta>
+{
+    public RepositorioContaEmArquivo(ContextoDados contextoDados) : base(contextoDados)
+    {
+    }
+
+    protected override List<Conta> ObterRegistros()
+    {
+        return contextoDados.Contas;
+    }
+
+    public List<Conta> SelecionarContasPorData(DateTime dataFaturamento)
+    {
+        List<Conta> contasDoDia = new List<Conta>();
+
+        foreach (Conta conta in registros)
+        {
+            if (conta.Fechamento.Date == dataFaturamento.Date)
+                contasDoDia.Add(conta);
+        }
+
+        return contasDoDia;
+    }
+
+    public List<Conta> SelecionarContasEmAberto()
+    {
+        List<Conta> contasEmAberto = new List<Conta>();
+
+        foreach (Conta conta in registros)
+        {
+            if (conta.EstaAberta)
+                contasEmAberto.Add(conta);
+        }
+
+        return contasEmAberto;
+    }
+
+    public List<Conta> SelecionarContasFechadas()
+    {
+        List<Conta> contasFechadas = new List<Conta>();
+
+        foreach (Conta conta in registros)
+        {
+            if (!conta.EstaAberta)
+                contasFechadas.Add(conta);
+        }
+
+        return contasFechadas;
+    }
+}
